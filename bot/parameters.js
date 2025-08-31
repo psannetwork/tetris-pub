cc1dc244d5db5f7fbf480d8e0bbdfadaeed6e788
psan <127632545+psannetwork@users.noreply.github.com>
2025-08-31 11:03:46 +0000

    a

--- bot/parameters.js
+++ bot/parameters.js
@@ -1,18 +1,18 @@
 const BASE_AI_PARAMETERS = {
-  "weightAggregateHeight": -0.03959237470712439,
-  "weightBumpiness": -0.12840783600609856,
-  "weightHoles": -17.843484265654197,
-  "weightUpperRisk": -6.969214050259171,
-  "weightMiddleOpen": 0.5769829681292855,
-  "weightLowerPlacement": 0.000039687603882314984,
-  "weightUpperPlacement": -0.09580430559955672,
-  "weightEdgePenalty": -0.25542562477404135,
-  "holeDepthFactor": 0.27758206945095437,
-  "lowerHoleFactor": 0.003978454214545634,
-  "contiguousHoleFactor": 1.2867019059670122,
-  "maxHeightPenaltyFactor": 0.015385487773777616,
-  "bumpinessFactor": 8.880265814150675,
-  "wellFactor": -0.7105037936617853
+  "weightAggregateHeight": -0.0023898858365009107,
+  "weightBumpiness": -0.08694760351155076,
+  "weightHoles": -14.729225607524917,
+  "weightUpperRisk": -5.558982706775383,
+  "weightMiddleOpen": 0.6156777439022475,
+  "weightLowerPlacement": 0.00007882663453159237,
+  "weightUpperPlacement": -0.0858374155813912,
+  "weightEdgePenalty": -0.37684529847362624,
+  "holeDepthFactor": 0.054228906218292065,
+  "lowerHoleFactor": 0.007435928195929443,
+  "contiguousHoleFactor": 2.03219045347795,
+  "maxHeightPenaltyFactor": 0.013286585765184168,
+  "bumpinessFactor": 11.421315577229617,
+  "wellFactor": -1.1755637067833866
 };
 
 module.exports = { BASE_AI_PARAMETERS };
